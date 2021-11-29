0434bde8bca1bc0117d2d8cbb8eafb745dbb327d
czrpb <czrpb@yahoo.com>
2021-11-28 17:25:28 -0800

    3 exercises

---
+++ exercism/resistor-color/resistor-color.ts
@@ -0,0 +1,9 @@
+export function colorCode(color: string): number {
+  return COLORS.indexOf(color);
+}
+
+export const COLORS: string[] = [
+  "black", "brown", "red", "orange",
+  "yellow", "green", "blue",
+  "violet", "grey", "white"
+]
